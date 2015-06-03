50febc352de147d9f22aebc02a24ef91a1b445aa
csaunders1976 <chelsiesaunders@Chelsies-MacBook-Pro-3.local>
2015-06-02 18:37:58 -0600

    finished canvas videos

--- HTML5 Canvas/1-Starting/js/Compositing.js
+++ HTML5 Canvas/1-Starting/js/Compositing.js
@@ -13,13 +13,38 @@ window.onload = function() {
 					{x:20, y:20, width:75, height:75},
 					{x: 40, y:50, width: 75, height: 75}
 			
-					];	
-			
-			
-			
-	
-			
-											
+					];
+
+
+		var method=0;
+
+		for(var i= 0; i<compMethods.length; i++){
+
+			var cvsName = "Canvas" + (i+1);
+			var curCanvas = document.getElementById(cvsName);
+			var curContext = curCanvas.getContext("2d");
+
+			curContext.fillStyle = "blue";
+
+			curContext.fillRect(rects[0].x, rects[0].y, rects[0].width, rects[0].height);
+			curContext.globalCompositeOperation =compMethods[method++];
+
+
+
+			curContext.fillStyle="green";
+			curContext.fillRect(rects[1].x, rects[1].y, rects[1].width, rects[1].height);
+
+
+
 		}
+
+
+
+
+
+
+
+
+	}
 	}
 }
